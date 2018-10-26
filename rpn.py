0d69db042b92db6bf9fb472a4e272f8c7cdfda17
Jason Kathawa <jkathawa@umich.edu>
2018-10-26 11:29:12 -0400

    added / and * initial

--- rpn.py
+++ rpn.py
@@ -20,6 +20,10 @@ def calculate(arg):
                 result = val2 + val1
             if token == '-':
                 result = val2 - val1
+            if token == '*':
+                result = val2 * val1
+            if token == '/'
+                result = val2 / val1
 
             stack.append(result)
             return stack[0]
