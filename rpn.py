a509d400b7e4e88d925c99c64e0881d6cee37c8b
Jason Kathawa <jkathawa@umich.edu>
2018-10-26 11:27:16 -0400

    added subtract

--- rpn.py
+++ rpn.py
@@ -16,7 +16,11 @@ def calculate(arg):
         except ValueError:
             val1 = stack.pop()
             val2 = stack.pop()
-            result = val1 + val2
+            if token == '+':
+                result = val2 + val1
+            if token == '-':
+                result = val2 - val1
+
             stack.append(result)
             return stack[0]
             
