54d8e013f7144f350d2d7c7f597af80f5f50c169
Jason Kathawa <jkathawa@umich.edu>
2018-10-26 11:31:14 -0400

    added but not ran multiply and divide tests

--- test_rpn.py
+++ test_rpn.py
@@ -8,5 +8,11 @@ class TestBasics(unittest.TestCase):
     def test_sub(self):
         result = rpn.calculate('4 3 -')
         self.assertEqual(1, result)
+    def test_mul(self):
+        result = rpn.calculate('5 -2 *')
+        self.assertEqual(-10, result)
+    def test_div(self):
+        result = rpn.calculate('-10 5 /')
+        self.assertEqual(-2, result)
 
 # TODO: add a subtract test 
